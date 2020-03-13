a6d499785730983aa577e58537dad1fa23f8cf8f
github username <github email address>
2020-03-13 15:45:09 -0400

    commit

--- app/models/shuttle.rb
+++ app/models/shuttle.rb
@@ -1,3 +1,13 @@
 class Shuttle
-    
+    attr_reader :model, :capacity 
+    @@all = []
+
+    def initialize(model, capacity)
+        @model = model 
+        @capacity = capacity 
+        Shuttle.all << self 
+    end 
+    def self.all 
+        @@all 
+    end 
 end
