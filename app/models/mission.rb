d8605522d68d712f167eb8c2cf3f936f05425603
duruchima <duruchima1@gmail.com>
2020-03-13 15:31:59 -0400

    1st commit from Duncan, created classes

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,11 @@
 class Mission
+    @@all = []
+    attr_accessor :launch_date
+    def initialize(launch_date)
+        @launch_date = launch_date
+    end
 
+    def self.all
+        @@all
+    end
 end
